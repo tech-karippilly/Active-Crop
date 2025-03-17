a118fb9d5457670272d5596262110e13b15a3650
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-03-17 20:28:05 +0530

    sesson created

--- src/app.js
+++ src/app.js
@@ -8,8 +8,11 @@ import passport from 'passport'
 
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extends: true }))
+
 app.use(function (req, res, next) {
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     next();
