d7838081315f9f47d6f35b6fb7227f814ba6581b
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-03-18 22:36:11 +0530

    revert changes

--- src/app.js
+++ src/app.js
@@ -83,11 +83,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files correctly
-// app.use('/public', express.static(path.join(__dirname, 'public')));
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'uploads')));
+
 
 // SWAGGER DOCS
 const combinedSwaggerSpec = {
