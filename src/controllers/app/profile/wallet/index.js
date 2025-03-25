a71774874db2a4ff43dfaff7c24180327c0fc733
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-03-25 16:25:25 +0530

    referal link copy added deleted unwanted file

--- src/controllers/app/profile/wallet/index.js
+++ src/controllers/app/profile/wallet/index.js
@@ -17,7 +17,8 @@ async function renderWalletPage(req, res) {
             const userId = jwtDecode.userId
             const currentUser = await User.findById(userId)
             const wallet = await Wallet.findOne({ userId })
-            const transactions = await Transactions.find({type:'wallet'}).sort({createdAt: -1}).limit(10)
+
+            const transactions = await Transactions.find({type:'wallet',walletId:wallet._id}).sort({createdAt: -1}).limit(10)
             if (!wallet) {
                 const newWallet = {
                     userId: userId
