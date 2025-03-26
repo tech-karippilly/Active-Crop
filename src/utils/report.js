99524797b16ad2f2ef269905fc2a01c11985b36a
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-03-26 19:59:45 +0530

    report update added

--- src/utils/report.js
+++ src/utils/report.js
@@ -3,65 +3,68 @@ import { Order } from "../models/index.js";
 
 
 const getOrdersBySpecificDate = async (dateString) => {
-    try {
-        const startDate = moment(dateString).startOf("day").toDate();
-        const endDate = moment(dateString).endOf("day").toDate();
-        const orders = await Order.aggregate([
-            {
-                $match: {
-                    createdAt: { $gte: startDate, $lte: endDate }
-                }
-            },
-            {
-                $sort: { createdAt: -1 }
-            }
-        ]);
-        return orders;
-    } catch (error) {
-        console.error("Error fetching orders:", error);
-    }
+  try {
+    const startDate = moment(dateString).startOf("day").toDate();
+    const endDate = moment(dateString).endOf("day").toDate();
+    const orders = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: startDate, $lte: endDate },
+          deliveryStatus: 'Delivered'
+        }
+      },
+      {
+        $sort: { createdAt: -1 }
+      }
+    ]);
+    return orders;
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+  }
 }
 
 const getMonthlyReportForSpecificMonth = async (monthString) => {
-    try {
-      const startDate = moment(monthString, "YYYY-MM").startOf("month").toDate();
-      const endDate = moment(monthString, "YYYY-MM").endOf("month").toDate(); 
+  try {
+    const startDate = moment(monthString, "YYYY-MM").startOf("month").toDate();
+    const endDate = moment(monthString, "YYYY-MM").endOf("month").toDate();
 
-      const order = await Order.aggregate([
-        {
-          $match: {
-            createdAt: { $gte: startDate, $lte: endDate } 
-          }
+    const order = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: startDate, $lte: endDate },
+          deliveryStatus: 'Delivered'
         }
-      ]);
-      
-      return order
-    } catch (error) {
-      console.error("Error generating monthly report:", error);
-    }
-  };
+      }
+    ]);
 
-  const getOrdersBetweenDates = async (startDateString, endDateString) => {
-    try {
-      const startDate = moment(startDateString, "YYYY-MM-DD").startOf("day").toDate(); 
-      const endDate = moment(endDateString, "YYYY-MM-DD").endOf("day").toDate(); 
-  
-      const orders = await Order.aggregate([
-        {
-          $match: {
-            createdAt: { $gte: startDate, $lte: endDate } 
-          }
+    return order
+  } catch (error) {
+    console.error("Error generating monthly report:", error);
+  }
+};
+
+const getOrdersBetweenDates = async (startDateString, endDateString) => {
+  try {
+    const startDate = moment(startDateString, "YYYY-MM-DD").startOf("day").toDate();
+    const endDate = moment(endDateString, "YYYY-MM-DD").endOf("day").toDate();
+
+    const orders = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: startDate, $lte: endDate },
+          deliveryStatus: 'Delivered'
         }
-      ]);
-  
-      return orders
-    } catch (error) {
-      console.error("Error fetching orders:", error);
-    }
-  };
+      }
+    ]);
+
+    return orders
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+  }
+};
 
 export {
-    getOrdersBySpecificDate,
-    getMonthlyReportForSpecificMonth,
-    getOrdersBetweenDates
+  getOrdersBySpecificDate,
+  getMonthlyReportForSpecificMonth,
+  getOrdersBetweenDates
 }
